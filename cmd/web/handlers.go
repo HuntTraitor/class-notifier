0ec7a39951e6c8a8c9bca1462049d3409903e108
hunttraitor <hunterrrisatratar@gmail.com>
2024-01-08 14:42:27 -0800

    Added routes for user login functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -133,3 +133,23 @@ func (app *application) viewNotifications(w http.ResponseWriter, r *http.Request
 		fmt.Fprintf(w, "%+v\n", class)
 	}
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display an HTML form for signing up a new user...")
+}
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display an HTML form for logging in a user...")
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login in user...")
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
