4caa3c5cb99a7efdc485d2699150f6ff9facb674
hunttraitor <hunterrrisatratar@gmail.com>
2024-01-29 16:22:10 -0800

    Added integration tests for the rest of the users model

--- internal/models/users_test.go
+++ internal/models/users_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/hunttraitor/class-notifier/internal/assert"
 )
 
-func TestUserModelExists(t *testing.T) {
 
+func TestUserModelExists(t *testing.T) {
 	//-short flag skips test
 	if testing.Short() {
 		t.Skip("models: skipping integration test")
@@ -42,6 +42,91 @@ func TestUserModelExists(t *testing.T) {
 			exists, err := m.Exists(tt.userID)
 			assert.Equal(t, exists, tt.want)
 			assert.NilError(t, err)
+		})
+	}
+}
+
+func TestUserModelInsert(t *testing.T) {
+	if testing.Short() {
+		t.Skip("models: skpping integration tests")
+	}
+
+	tests := []struct {
+		name string
+		userName string
+		email string
+		password string
+		wantError error
+	}{
+		{
+			name: "Valid Insert",
+			userName: "Test User",
+			email: "test@gmail.com",
+			password: "pa$$word",
+			wantError: nil,
+		},
+		{
+			name: "Duplicate Insert",
+			userName: "Hunter",
+			email: "hunter@gmail.com",
+			password: "pa$$word",
+			wantError: ErrDuplicateEmail,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := newTestDB(t)
+			m := UserModel{db}
+			err := m.Insert(tt.userName, tt.email, tt.password)
+			assert.Equal(t, err, tt.wantError)
+		})
+	}
+}
+
+func TestUserModelAuthenticate(t *testing.T) {
+	if testing.Short() {
+		t.Skip("models: skipping integration tests")
+	}
+
+	tests := []struct {
+		name string
+		email string
+		password string
+		wantUserID int
+		wantError error
+	}{
+		{
+			name: "Valid Authentication",
+			email: "testuserauth@gmail.com",
+			password: "pa$$word",
+			wantUserID: 2,
+			wantError: nil,
+		},
+		{
+			name: "Invalid email",
+			email: "invalidEmail@gmail.com",
+			password: "pa$$word",
+			wantUserID: 0,
+			wantError: ErrInvalidCredentials,
+		},
+		{
+			name: "Invalid Password",
+			email: "testuserauth@gmail.com",
+			password: "wrongPassword",
+			wantUserID: 0,
+			wantError: ErrInvalidCredentials,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := newTestDB(t)
+			m := UserModel{db}
+			m.Insert("test", "testuserauth@gmail.com", "pa$$word")
+			userID, err := m.Authenticate(tt.email, tt.password)
+			assert.Equal(t, userID, tt.wantUserID)
+			assert.Equal(t, err, tt.wantError)
 		})
 	}
 }
